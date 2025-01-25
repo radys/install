dcd543afc43314612d4dc50e0f8c7d2ef27d64a4
Radomír Orkáč <radomir@orkac.cz>
2025-01-25 10:43:45 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ restore-config /mnt/grml/scripts/config.tbz
 ifup wlan0
 apt update
 apt install -y timeshift
+
+wget -O /root/install.sh ces.net/install
+chmod +x /root/install.sh
+echo '#!/bin/bash' > /root/up.sh
+echo 'ifdown wlan0 ; ifup wlan0' >> /root/up.sh
+chmod +x /root/up.sh
